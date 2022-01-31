d63e7269d234f81d9342b8ff2a5b10645c15a8cf
sangsang-6914 <sangsang6914@gmail.com>
2022-01-31 23:25:54 +0900

    canban test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import React from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { hourSelector, minuteState } from './atoms';
+
 
 function App() {
-  const [minutes, setMinutes] = useRecoilState(minuteState)
-  const [hours, setHours] = useRecoilState(hourSelector)
-  const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
-    setMinutes(+event.currentTarget.value)
-  }
-  const onHourChange = (event:React.FormEvent<HTMLInputElement>) => {
-    setHours(+event.currentTarget.value)
-  }
   return (
     <div>
-      <input value={minutes} onChange={onMinutesChange} type="number" placeholder='Minutes' />
-      <input value={hours} onChange={onHourChange} type="number" placeholder='Minutes' />
+      <h1>Canban</h1>
     </div>
-    )
+  )
 }
 
 export default App;
